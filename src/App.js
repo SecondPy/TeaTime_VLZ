b8a8c6ff85372ad68c9b3057f38cd725b6c9d23c
SecondPy <78914221+SecondPy@users.noreply.github.com>
2023-09-01 12:12:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <form id = "form">  
-        <button id="order">Забронировать</button>
-        </form>
-    <script src="https://telegram.org/js/telegram-web-app.js"></script>
         <h1>Добро пожаловать в чайный клуб TeaTime</h1>
         <TableSelection />
         <img src={logo} className="App-logo" alt="logo" />
